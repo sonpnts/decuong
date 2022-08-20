956e2fa0e9981936ea67bac640c4ef5253620238
kyllun <2151050223lan@ou.edu.vn>
2022-08-20 15:30:16 +0700

    update

--- assets/js/java.js
+++ assets/js/java.js
@@ -1,19 +1,34 @@
+
 $(document).ready(function(){
-    $(".img-yt span").click(function () {
-        $(".play-yt").addClass("open");
+    const $ = document.querySelector.bind(document);
+    const $$ = document.querySelectorAll.bind(document);
+
+    const tabs = $$(".tab-item");
+    const panes = $$(".tab-pane");
+
+    const tabActive = $(".tab-item.active");
+    const line = $(".tabs .line");
+
+    requestIdleCallback(function () {
+        line.style.left = tabActive.offsetLeft + "px";
+        line.style.width = tabActive.offsetWidth + "px";
     });
-    $("#close").click(function(){
-        $(".play-yt").removeClass("open");
+
+    tabs.forEach((tab, index) => {
+        const pane = panes[index];
+
+        tab.onclick = function () {
+        $(".tab-item.active").classList.remove("active");
+        $(".tab-pane.active").classList.remove("active");
+
+        line.style.left = this.offsetLeft + "px";
+        line.style.width = this.offsetWidth + "px";
+
+        this.classList.add("active");
+        pane.classList.add("active");
+        };
     });
-    $(".play-yt").click(function () {
-        $(".play-yt").removeClass("open");
-    });
-    $(".play-yt").click(function () {
-        $("#video-yt").remove();
-    });
-    $("#close").click(function(){
-        $("#video-yt").remove();
-    });
+    //////////////////////////////////////////////////
     $(".animate__animated").hover(function(){
         $(this).addClass("animate__bounce");
     }, function(){
@@ -31,4 +46,13 @@ $(document).ready(function(){
             scrollTop:0
         },500);
     });
+    $(window).scroll(function(){
+        if($(this).scrollTop()){
+            $(".menu-scroll").fadeIn();
+            $("#header").fadeOut();
+        }else{
+            $(".menu-scroll").fadeOut();
+            $("#header").fadeIn();
+        }
+    })
 });
