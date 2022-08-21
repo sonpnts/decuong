35a5bf7ad1ed016fbbd3e0e27f6d4171cbf15887
kyllun <2151050223lan@ou.edu.vn>
2022-08-21 21:57:10 +0700

    update

--- assets/js/java.js
+++ assets/js/java.js
@@ -1,13 +1,13 @@
-
 $(document).ready(function(){
-    const $ = document.querySelector.bind(document);
-    const $$ = document.querySelectorAll.bind(document);
+    
+    const $$ = document.querySelector.bind(document);
+    const $$$ = document.querySelectorAll.bind(document);
 
-    const tabs = $$(".tab-item");
-    const panes = $$(".tab-pane");
+    const tabs = $$$(".tab-item");
+    const panes = $$$(".tab-pane");
 
-    const tabActive = $(".tab-item.active");
-    const line = $(".tabs .line");
+    const tabActive = $$(".tab-item.active");
+    const line = $$(".tabs .line");
 
     requestIdleCallback(function () {
         line.style.left = tabActive.offsetLeft + "px";
@@ -18,8 +18,8 @@ $(document).ready(function(){
         const pane = panes[index];
 
         tab.onclick = function () {
-        $(".tab-item.active").classList.remove("active");
-        $(".tab-pane.active").classList.remove("active");
+        $$(".tab-item.active").classList.remove("active");
+        $$(".tab-pane.active").classList.remove("active");
 
         line.style.left = this.offsetLeft + "px";
         line.style.width = this.offsetWidth + "px";
@@ -28,7 +28,7 @@ $(document).ready(function(){
         pane.classList.add("active");
         };
     });
-    //////////////////////////////////////////////////
+
     $(".animate__animated").hover(function(){
         $(this).addClass("animate__bounce");
     }, function(){
